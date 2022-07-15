9ba11c785696057aa99505a397333b2293ed0163
IshaKansal-25 <isha25.kansal@gmail.com>
2022-07-15 11:37:30 +0530

    assignments code

---
+++ Assignment-4.js
@@ -0,0 +1,13 @@
+var x=eval(prompt("enter first number"));
+    var y=eval(prompt("enter second number"));
+    var sum=x+y;
+    var sub=x-y;
+
+    document.write(sum);
+    document.write(sub);
+
+    console.log(sum);
+    console.log(sub);
+
+    alert(sum);
+    alert(sub);
